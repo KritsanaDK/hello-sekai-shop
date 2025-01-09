9d06e60da0e062cbeb67a5020f98a367546ac939
kritsanawathaniyanon <kritsana.dk@gmail.com>
2025-01-09 07:31:06 +0700

    update

--- main.go
+++ main.go
@@ -1,3 +1,29 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/KaDingMeaw/hello-sekai-shop/config"
+	"github.com/KaDingMeaw/hello-sekai-shop/pkg/database"
+)
+
+func main() {
+	ctx := context.Background()
+
+	// Initialize config
+	cfg := config.LoadConfig(func() string {
+		if len(os.Args) < 2 {
+			log.Fatal("Error: .env path is required")
+		}
+		return os.Args[1]
+	}())
+
+	// Database connection
+	db := database.DbConn(ctx, &cfg)
+
+	defer db.Disconnect(ctx)
+	log.Println(db)
+
+}
